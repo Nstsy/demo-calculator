218c509a9ec1524f1036bc7bc498cbed4e1b1ef9
nstsy <nstsy@yandex.ru>
2024-10-17 13:45:24 +0300

    add method sum, subctraction

--- src/main/java/by/it/stolybko/calculator/Calculator.java
+++ src/main/java/by/it/stolybko/calculator/Calculator.java
@@ -1,4 +1,13 @@
 package by.it.stolybko.calculator;
 
 public class Calculator {
+    public double sum(int a, int b) {
+        return a + b;
+    }
+
+    public double subtraction(int a, int b) {
+        return a - b;
+    }
+
+
 }
