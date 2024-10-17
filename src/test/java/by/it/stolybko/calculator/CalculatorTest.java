e925110ef45aabde5ef7d229a90fd2694f294b9c
nstsy <nstsy@yandex.ru>
2024-10-17 14:09:40 +0300

    add all tests

--- src/test/java/by/it/stolybko/calculator/CalculatorTest.java
+++ src/test/java/by/it/stolybko/calculator/CalculatorTest.java
@@ -1,4 +1,32 @@
 package by.it.stolybko.calculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
+    @Test
+    public void testSumPositive() {
+        Assertions.assertEquals(5, calculator.sum(2,3));
+    }
+    @Test
+    public void testSumNegative() {
+        Assertions.assertEquals(-38, calculator.sum(-7,-31));
+    }
+    @Test
+    public void testSumZeroPozitive() {
+        Assertions.assertEquals(5, calculator.sum(0,5));
+    }
+    @Test
+    public void testSumZeroNegative() {
+        Assertions.assertEquals(-5, calculator.sum(0,-5));
+    }
+    @Test
+    public void testSumZeroNegativePositive() {
+        Assertions.assertEquals(-3, calculator.sum(2,-5));
+    }
+    @Test
+    public void testSumZeroZero() {
+        Assertions.assertEquals(0, calculator.sum(0,0));
+    }
 }
